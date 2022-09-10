b290724d93a1cc1ef0506275d6635c6b5cd556aa
SvenHamers <44347057+SvenHamers@users.noreply.github.com>
2022-09-10 18:51:21 +0200

    list usb devices

--- whr/src/whr930.py
+++ whr/src/whr930.py
@@ -13,6 +13,8 @@ import time
 import sys
 import serial
 import json
+import re
+import subprocess
 
 
 def debug_msg(message):
@@ -910,6 +912,20 @@ def main():
     debug = False
     debug_level = 0
     warning = False
+
+
+    device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+    df = subprocess.check_output("lsusb")
+    devices = []
+    for i in df.split(b'\n'):
+        if i:
+            info = device_re.match(i)
+            if info:
+                dinfo = info.groupdict()
+                dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
+                devices.append(dinfo)
+                
+    print(devices)
 
 
     # Opening JSON file
